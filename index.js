11b7a16b7c3f23ea924e83d5b94796b805d97df0
creativerze <creativerze@gmail.com>
2022-10-26 21:00:24 +0600

    added details dynamic routes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const details = require('./data/details.json');
 
 app.get('/', (req, res) => {
   res.send('its work');
@@ -14,6 +15,22 @@ app.get('/', (req, res) => {
 
 app.get('/all-courses', (req, res) => {
   res.send(courses);
+});
+
+app.get('/details', (req, res) => {
+  res.send(details);
+});
+
+app.get('/details/:id', (req, res) => {
+  const id = req.params.id;
+  const course_details = details.filter(d => d.course_id === id);
+  res.send(course_details);
+});
+
+app.get('/all-details/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = details.find(d => d._id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
